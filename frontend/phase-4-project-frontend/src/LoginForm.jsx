3e5170dc32e22572f1a42e1fb2334b9f5ba0e773
Daniel Morley <morleydan10@gmail.com>
2024-01-18 16:35:12 -0500

    frontend basic setup

---
+++ frontend/phase-4-project-frontend/src/LoginForm.jsx
@@ -0,0 +1,13 @@
+
+function LoginForm() {
+
+    return(
+        <form id='login-form'>
+                <input type='text' placeholder="Username">Username</input>
+                <input type='text' placeholder="Password">Password</input>
+                <input type='submit'>Submit</input>
+        </form>
+    )
+}
+
+export default LoginForm;
