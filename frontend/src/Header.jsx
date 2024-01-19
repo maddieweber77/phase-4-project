6b4e92f3f6c3b46e0c27328acca101fa426968c4
Maddie Weber <maddieweber7@gmail.com>
2024-01-19 11:34:26 -0500

    added caption_meme

--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -5,6 +5,10 @@ function Header(){
     return(
         <>
         <h1 id='app-title'>MEMEWARS</h1>
+        <button>Home</button>
+        {/* if the user is signed in, below button should be "Profile" but otherwise should be "Sign In". We also need to redirect them to either the signin page or the profile depending on the button*/}
+        <button>Profile</button>
+
         </>
     )
 }
