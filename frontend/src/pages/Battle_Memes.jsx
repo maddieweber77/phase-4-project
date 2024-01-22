121ff5b39d3504c56b5052d8bab403183c20b5a2
Maddie Weber <maddieweber7@gmail.com>
2024-01-22 12:38:18 -0500

    got captions working

--- frontend/src/pages/Battle_Memes.jsx
+++ frontend/src/pages/Battle_Memes.jsx
@@ -9,17 +9,15 @@ function Battle_Memes() {
     // featured meme - these should originally just be set to a random meme in our database
     const [featuredMeme1, setFeaturedMeme1] = useState({});
     const [featuredMeme2, setFeaturedMeme2] = useState({});
-
     // featured captions -- these should initially be set to random captions (within a given meme)
     const [featuredCap1, setFeaturedCap1] = useState({});
     const [featuredCap2, setFeaturedCap2] = useState({});
-
     const [newDescription, setNewDescription] = useState(""); // Define newDescription state
     const [caption, setCaption] = useState(""); // Define caption state
-
     const [totalMemes, setTotalMemes] = useState(0); // Use state to track # of totalMemes
-
     const [memes, setMemes] = useState([]); // Added state for storing meme data
+    const [description1, setDescription1] = useState(""); // New state for description
+    const [description2, setDescription2] = useState(""); // New state for description
 
     useEffect(() => {
         // Fetch the list of memes when the component mounts
@@ -30,7 +28,11 @@ function Battle_Memes() {
                 setTotalMemes(data.length);
                 //! need to do a random number
                 setFeaturedMeme1(data[0]); // Set initial featured memes
-                setFeaturedMeme2(data[1]);
+                setFeaturedMeme2(data[0]);
+                setFeaturedCap1(data[0]); // Assuming there is at least one response
+                setFeaturedCap2(data[0]); // Assuming there are at least two responses
+                setDescription1("");
+                setDescription2("");
             })
             .catch(error => {
                 console.error("Error fetching memes:", error);
@@ -38,42 +40,29 @@ function Battle_Memes() {
     }, []); 
 
     const showNextMeme = () => {
+        //! we need to make sure that it's a different # from last time
         const currentIndex = Math.floor(Math.random() * totalMemes);
         const nextIndex = (currentIndex + 1) % totalMemes;
         setFeaturedMeme1(memes[nextIndex]);
         setFeaturedMeme2(memes[nextIndex]);
-    };
 
-    const handleCaptionSubmit = () => {
-        const memeId = featuredMeme1.id; // Assuming id is the property containing the ID
-    
-        fetch(`http://localhost:3000/memes/${memeId}/descriptions`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                description: newDescription,
-            }),
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log("Description added:", data);
-        })
-        .catch(error => {
-            console.error("Error adding description:", error);
-        });
-    
-        showNextMeme();
-        setCaption("");
+        // Set responses for the next memes
+        setFeaturedCap1(memes[nextIndex]); // Assuming there is at least one response
+        setFeaturedCap2(memes[nextIndex]); // Assuming there are at least two responses
+
+        setDescription1("");
+        setDescription2("");
     };
 
     return (
         <main>
             <Header />
             <div>
-                <img src={featuredMeme1.img_url} onClick={handleCaptionSubmit} width="100%" alt="Meme" />
-                <img src={featuredMeme2.img_url} onClick={handleCaptionSubmit} width="100%" alt="Meme" />
+                <img src={featuredMeme1.img_url} onClick={showNextMeme} width="100%" alt="Meme" />
+                <p>{featuredCap1.caption}</p>
+
+                <img src={featuredMeme2.img_url} onClick={showNextMeme} width="100%" alt="Meme" />
+                <p>{featuredCap2.caption}</p>
             </div>
 
             <button>Submit</button>
