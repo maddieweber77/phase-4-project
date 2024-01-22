b807bb612890ee9f32ef23309c85c87df847bf16
Maddie Weber <maddieweber7@gmail.com>
2024-01-22 13:23:03 -0500

    now captions are mostly showing up

--- frontend/src/pages/Battle_Memes.jsx
+++ frontend/src/pages/Battle_Memes.jsx
@@ -10,34 +10,43 @@ function Battle_Memes() {
     const [featuredMeme1, setFeaturedMeme1] = useState({});
     const [featuredMeme2, setFeaturedMeme2] = useState({});
     // featured captions -- these should initially be set to random captions (within a given meme)
-    const [featuredCap1, setFeaturedCap1] = useState({});
-    const [featuredCap2, setFeaturedCap2] = useState({});
-    const [newDescription, setNewDescription] = useState(""); // Define newDescription state
+    const [featuredCap1, setFeaturedCap1] = useState("");
+    const [featuredCap2, setFeaturedCap2] = useState("");
+    const [dataLoaded, setDataLoaded] = useState(false);
     const [caption, setCaption] = useState(""); // Define caption state
     const [totalMemes, setTotalMemes] = useState(0); // Use state to track # of totalMemes
     const [memes, setMemes] = useState([]); // Added state for storing meme data
-    const [description1, setDescription1] = useState(""); // New state for description
-    const [description2, setDescription2] = useState(""); // New state for description
+    const [responses, setResponses] = useState([]);
 
     useEffect(() => {
-        // Fetch the list of memes when the component mounts
-        fetch("http://localhost:3000/memes")
-            .then(response => response.json())
-            .then(data => {
-                setMemes(data);
-                setTotalMemes(data.length);
-                //! need to do a random number
-                setFeaturedMeme1(data[0]); // Set initial featured memes
-                setFeaturedMeme2(data[0]);
-                setFeaturedCap1(data[0]); // Assuming there is at least one response
-                setFeaturedCap2(data[0]); // Assuming there are at least two responses
-                setDescription1("");
-                setDescription2("");
-            })
-            .catch(error => {
-                console.error("Error fetching memes:", error);
-            });
-    }, []); 
+        // Fetch the list of responses and memes when the component mounts
+        Promise.all([
+          fetch("http://localhost:3000/responses").then((response) =>
+            response.json()
+          ),
+          fetch("http://localhost:3000/memes").then((response) => response.json()),
+        ])
+          .then(([responsesData, memesData]) => {
+            setResponses(responsesData);
+            setMemes(memesData);
+            setTotalMemes(memesData.length);
+    
+            const randomIndex = Math.floor(Math.random() * memesData.length);
+            const initialMeme = memesData[randomIndex];
+    
+            setFeaturedMeme1(initialMeme);
+            setFeaturedMeme2(initialMeme);
+    
+            const initialResponse = getResponseByMemeId(initialMeme.id);
+            setFeaturedCap1(initialResponse);
+            setFeaturedCap2(initialResponse);
+
+            setDataLoaded(true);
+          })
+          .catch((error) => {
+            console.error("Error fetching data:", error);
+          });
+      }, []); 
 
     const showNextMeme = () => {
         //! we need to make sure that it's a different # from last time
@@ -47,28 +56,43 @@ function Battle_Memes() {
         setFeaturedMeme2(memes[nextIndex]);
 
         // Set responses for the next memes
-        setFeaturedCap1(memes[nextIndex]); // Assuming there is at least one response
-        setFeaturedCap2(memes[nextIndex]); // Assuming there are at least two responses
+        setFeaturedCap1(getResponseByMemeId(memes[nextIndex].id));
+        setFeaturedCap2(getResponseByMemeId(memes[nextIndex].id));
 
-        setDescription1("");
-        setDescription2("");
+    };
+
+    const getResponseByMemeId = (memeId) => {
+        const responsesForMeme = responses.filter(response => response.meme_id === memeId);
+        console.log("printing from getResponseByMemeId:")
+        console.log(responsesForMeme[0]?.response)
+        return responsesForMeme[0]?.response || "";
     };
 
     return (
         <main>
-            <Header />
-            <div>
-                <img src={featuredMeme1.img_url} onClick={showNextMeme} width="100%" alt="Meme" />
-                <p>{featuredCap1.caption}</p>
-
-                <img src={featuredMeme2.img_url} onClick={showNextMeme} width="100%" alt="Meme" />
-                <p>{featuredCap2.caption}</p>
-            </div>
-
-            <button>Submit</button>
-            <button onClick={showNextMeme}>See Next Meme</button>
+          <Header />
+          <div>
+            <img
+              src={featuredMeme1.img_url}
+              onClick={showNextMeme}
+              width="100%"
+              alt="Meme"
+            />
+            {dataLoaded && <p>{featuredCap1}</p>}
+    
+            <img
+              src={featuredMeme2.img_url}
+              onClick={showNextMeme}
+              width="100%"
+              alt="Meme"
+            />
+            {dataLoaded && <p>{featuredCap2}</p>}
+          </div>
+    
+          <button>Submit</button>
+          <button onClick={showNextMeme}>See Next Meme</button>
         </main>
-    );
+      );
 }
 
 export default Battle_Memes;
