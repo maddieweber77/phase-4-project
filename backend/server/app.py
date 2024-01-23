73cf9fc908857370c1eb27b1d13c12183d47debe
Ethan Hessler <ethanhessler@gmail.com>
2024-01-23 13:10:05 -0500

    working on routes

--- backend/server/app.py
+++ backend/server/app.py
@@ -2,22 +2,22 @@ from flask import Flask, request, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
-from flask_bcrypt import Bcrypt
-from dotenv import dotenv_values
-from models import db, User, Connection, Meme, Response
-config = dotenv_values(".env")
+# from flask_bcrypt import Bcrypt
+# from dotenv import dotenv_values
+from models import db, User, Connection, Meme, Response, Ballot
+# config = dotenv_values(".env")
 
 
 from models import db
 
 app = Flask(__name__)
-app.secret_key = config['FLASK_SECRET_KEY']
-# NEED SECRET KEY PAGE
-CORS(app)
+# # app.secret_key = config['FLASK_SECRET_KEY']
+# # NEED SECRET KEY PAGE
+# CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
-bcrypt = Bcrypt(app)
+# app.json.compact = False
+# bcrypt = Bcrypt(app)
 migrate = Migrate(app, db)
 
 db.init_app(app)
@@ -26,27 +26,43 @@ db.init_app(app)
 def index():
     return "Hello world"
 
+#User Routes
+@app.get('/User/<int:id>')
+def get_user_by_id(id):
+    user = db.session.get(User, id)
+    if not user:
+        return {"error": "no user with that ID"}, 404
+    return user.to_dict(rules = ['-ballots']), 200
+
+
+
+
+
+
+
+
+
 # ***************Authentication GET, POST, and DELETE requests**********************
-@app.get('/check_session')
-def check_session():
-    user = db.session.get(User, session.get(id))
-    # print to check the session object
-    print(session)
+# @app.get('/check_session')
+# def check_session():
+#     user = db.session.get(User, session.get(id))
+#     # print to check the session object
+#     print(session)
 
-    if user:
-        return user.to_dict(['-password_hash']), 200
+#     if user:
+#         return user.to_dict(['-password_hash']), 200
 
-    return {}
+#     return {}
 
-@app.delete('/logout')
-def logout():
+# @app.delete('/logout')
+# def logout():
 
-    try: 
-        session.pop(id)
-        return {"Message": "Logged out"}, 200
+#     try: 
+#         session.pop(id)
+#         return {"Message": "Logged out"}, 200
     
-    except:
-        return {'Message': 'No user logged in'}, 404
+#     except:
+#         return {'Message': 'No user logged in'}, 404
 
 
 
