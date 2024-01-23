679c2da4a41df8494a69d450c2e1349dd6036483
Maddie Weber <maddieweber7@gmail.com>
2024-01-23 15:02:33 -0500

    working on css

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -85,8 +85,12 @@ function Home() {
             {/* <Created_Cards userMemes={userMemes}/> */}
             {/* <Caption_Cards memesToBeCaptioned = {memesToBeCaptioned}/> */}
             {/* <Vote_Cards memesToBeVotedOn = {memesToBeVotedOn}/> */}
-            <Vote_Cards/>
-            <Leaderboard completedMemes={completedMemes}/>
+            <div className="vote-cards-container-bigger">
+            <Vote_Cards />
+            </div>
+            <div className="leaderboard-container">
+            <Leaderboard completedMemes={completedMemes} />
+            </div>
             {/* <Friend_Cards friends = {friends} notFriends = {notFriends}/> */}
 
 
