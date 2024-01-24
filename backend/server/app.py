b4e89e15a70408935677e41ee6ae9086a59b0ced
Ethan Hessler <ethanhessler@gmail.com>
2024-01-24 12:58:20 -0500

    dan changes

--- backend/server/app.py
+++ backend/server/app.py
@@ -2,22 +2,22 @@ from flask import Flask, request, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
-# from flask_bcrypt import Bcrypt
-# from dotenv import dotenv_values
+from flask_bcrypt import Bcrypt
+from dotenv import dotenv_values
 from models import db, User, Connection, Meme, Response, Ballot
-# config = dotenv_values(".env")
+config = dotenv_values(".env")
 
 
 from models import db
 
 app = Flask(__name__)
-# # app.secret_key = config['FLASK_SECRET_KEY']
+app.secret_key = config['FLASK_SECRET_KEY']
 # # NEED SECRET KEY PAGE
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
-# bcrypt = Bcrypt(app)
+bcrypt = Bcrypt(app)
 migrate = Migrate(app, db)
 
 db.init_app(app)
@@ -327,43 +327,43 @@ def delete_ballot_by_id(id):
 
 
 # ***************Authentication GET, POST, and DELETE requests**********************
-# @app.get('/check_session')
-# def check_session():
-#     user = db.session.get(User, session.get(id))
-#     # print to check the session object
-#     print(session)
+@app.get('/check_session')
+def check_session():
+    user = db.session.get(User, session.get(id))
+    # print to check the session object
+    print(session)
 
-#     if user:
-#         return user.to_dict(['-password_hash']), 200
+    if user:
+        return user.to_dict(['-password_hash']), 200
 
-#     return {}
+    return {}
 
-# @app.delete('/logout')
-# def logout():
+@app.delete('/logout')
+def logout():
 
-#     try: 
-#         session.pop(id)
-#         return {"Message": "Logged out"}, 200
+    try: 
+        session.pop(id)
+        return {"Message": "Logged out"}, 200
     
-#     except:
-#         return {'Message': 'No user logged in'}, 404
+    except:
+        return {'Message': 'No user logged in'}, 404
 
 
-@app.post('/login')
+@app.post('/api/login')
 def login():
     
     data = request.json
 
 
-#     user = User.query.filter(User.name == data.get('password')).first()
+    user = User.query.filter(User.name == data.get('password')).first()
 
-#     if user and bcrypt.check_password_hash(user.password_hash, data.get('password')):
-#         session["user_id"] = user.id
-#         print("success")
+    if user and bcrypt.check_password_hash(user.password_hash, data.get('password')):
+        session["user_id"] = user.id
+        print("success")
 
-#         return user.to_dict(), 200
-#     else:
-#         return {"error": "Invalid username or password"}, 401
+        return user.to_dict(), 200
+    else:
+        return {"error": "Invalid username or password"}, 401
 
 
 
